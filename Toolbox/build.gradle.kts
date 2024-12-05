f2645e39de72f05f8f76bd85a55ce8842e23c41c
Maksym Voloshko <maksimvoloshko02@gmail.com>
2024-12-05 22:49:32 +0200

    - updated sdk version

--- Toolbox/build.gradle.kts
+++ Toolbox/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.clyde.toolbox"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.clyde.toolbox"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.clyde.toolbox"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
